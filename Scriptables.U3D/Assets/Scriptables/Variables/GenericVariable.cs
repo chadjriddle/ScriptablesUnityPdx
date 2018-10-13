d045cd42c4f1bb5b7cfc0b74f92186e2f32c25b0
chadjriddle <chadjriddle@gmail.com>
2018-10-12 20:12:13 -0700

    Add Generic Variable

---
+++ Scriptables.U3D/Assets/Scriptables/Variables/GenericVariable.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using UnityEngine;
+
+namespace Scriptables.Variables
+{
+    [CreateAssetMenu(menuName = "Variables/Bool Variable")]
+    public class BoolVariable : GenericVariable<bool>
+    {
+    }
+
+    public abstract class GenericVariable<T> : ScriptableObject
+    {
+        [SerializeField]
+        private T _value;
+
+        public T Value => _value;
+
+        public event Action ValueChanged;
+
+        public virtual void SetValue(T value)
+        {
+            if (!Equals(_value, value))
+            {
+                _value = value;
+                OnValueChanged();
+            }
+        }
+
+        public void SetValue(GenericVariable<T> value)
+        {
+            SetValue(value.Value);
+        }
+
+        public static implicit operator T(GenericVariable<T> variable)
+        {
+            return variable.Value;
+        }
+
+        public virtual void ApplyChange(T amount)
+        {
+            SetValue(amount);
+        }
+
+        public virtual void ApplyChange(GenericVariable<T> amount)
+        {
+            SetValue(amount);
+        }
+
+        public virtual void OnValidate()
+        {
+            OnValueChanged();
+        }
+
+        protected void OnValueChanged()
+        {
+            var handler = ValueChanged;
+            handler?.Invoke();
+        }
+    }
+}
